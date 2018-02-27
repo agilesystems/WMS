4bcf81d32a2ee6131072baac7e60e5abbc72db51
Ramy Ashour <eng.ramy.ashour@gmail.com>
2018-02-27 19:03:19 +0200

    navbar still  has some issues (sub menus doesn't appear )

--- src/main/resources/static/app/controller/navbar-controller.js
+++ src/main/resources/static/app/controller/navbar-controller.js
@@ -2,5 +2,8 @@
 
 app.controller('navbar-controller', function ($http, $scope, $rootScope, $state) {
 
-
+$scope.parentMenuFilter = function (item) { 
+    console.log(item);
+    return item.userMenuCollection.length>0 || item.url!==null; 
+};
 });
