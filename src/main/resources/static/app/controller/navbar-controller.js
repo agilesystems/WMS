e8d3ed2345264c7ea4f263b56eff9aa6d749ded2
Ramy Ashour <eng.ramy.ashour@gmail.com>
2018-02-28 15:09:42 +0200

    Fix Navbar

--- src/main/resources/static/app/controller/navbar-controller.js
+++ src/main/resources/static/app/controller/navbar-controller.js
@@ -2,11 +2,12 @@
 
 app.controller('navbar-controller', function ($scope) {
 
-$scope.parentMenuFilter = function (item) { 
-    return item.menuCollection.length>0 || item.url!==null; 
-};
-$scope.subMenuFilter=function(item){
-    
-return  item.url!==null; }
+    $scope.parentMenuFilter = function (item) {
+        return item.menuCollection.length > 0 && item.url !== null;
+    };
+    $scope.subMenuFilter = function (item) {
+
+        return  item.url !== null;
+    };
 
 });
