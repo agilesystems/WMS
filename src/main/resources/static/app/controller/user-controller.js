89ab03426e2ae75268152535a65008c2ccb33f25
mhdsy <mhdsy@localhost.localdomain>
2018-02-28 22:48:03 +0200

    user controller

--- src/main/resources/static/app/controller/user-controller.js
+++ src/main/resources/static/app/controller/user-controller.js
@@ -8,22 +8,42 @@ function init($scope, $http, DataServiceApi) {
     DataServiceApi.GetData(server + 'menu/all').then(function (response) {
         $scope.menus = response.data;
     });
-
+$scope.user={menuCollection:[]};
     $scope.addUser = function () {
-        var data = {};
-        data.firstname = $scope.fname;
-        data.lastname = $scope.lname;
-        data.address = $scope.address;
-        data.username = $scope.username;
-        data.password = $scope.password;
-        data.phone = $scope.phone;
-        data.userMenuCollection = $('#lstBox1 option');
-//      console.log(data.userMenuCollection);
-        DataServiceApi.PostData(data, server + 'user/add');
+
+//        data.firstname=  $scope.fname;
+//        data.lastname = $scope.lname;
+//        data.address = $scope.address;
+//        data.username = $scope.username;
+//        data.password = $scope.password;
+//        data.phone = $scope.phone;
+//        
+//        var options = $('#lstBox1 option').each(function()
+//{
+//       
+//});
+
+
+        console.log($scope.user.menuCollection);
+        DataServiceApi.PostData($scope.user, server + 'user/add');
     };
-    
-    $scope.addAllRight= function(){
-        
+//    $scope.$watch('selected', function (nowSelected) {
+//        // reset to nothing, could use `splice` to preserve non-angular references
+//        $scope.user.menuCollection = [];
+//
+//        if (!nowSelected) {
+//            // sometimes selected is null or undefined
+//            return;
+//        }
+//
+//        // here's the magic
+//        angular.forEach(nowSelected, function (val) {
+//            $scope.user.menuCollection.push(val);
+//        });
+//    });
+
+    $scope.addAllRight = function () {
+
         var selectedOpts = $('#lstBox1 option');
         if (selectedOpts.length === 0) {
             alert("Nothing to move.");
@@ -31,21 +51,27 @@ function init($scope, $http, DataServiceApi) {
         }
         $('#lstBox2').append($(selectedOpts).clone());
         $(selectedOpts).remove();
+   
         e.preventDefault();
     };
-    
-    $scope.addOneRigth=function(){
-           var selectedOpts = $('#lstBox1 option:selected');
+            
+    $scope.addOneRigth = function () {
+        var selectedOpts = $('#lstBox1 option:selected.value');
         if (selectedOpts.length === 0) {
             alert("Nothing to move.");
+
             e.preventDefault();
         }
         $('#lstBox2').append($(selectedOpts).clone());
         $(selectedOpts).remove();
+        
+        var menu={ id:0};
+        menu.id=selectedOpts.option.value;
+        $scope.user.menuCollection.push(menu);
         e.preventDefault();
     };
-    $scope.addOneLeft=function(){
-         var selectedOpts = $('#lstBox2 option:selected');
+    $scope.addOneLeft = function () {
+        var selectedOpts = $('#lstBox2 option:selected');
         if (selectedOpts.length === 0) {
             alert("Nothing to move.");
             e.preventDefault();
@@ -54,8 +80,8 @@ function init($scope, $http, DataServiceApi) {
         $(selectedOpts).remove();
         e.preventDefault();
     };
-    $scope.addAllLeft=function(){
-         var selectedOpts = $('#lstBox2 option');
+    $scope.addAllLeft = function () {
+        var selectedOpts = $('#lstBox2 option');
         if (selectedOpts.length === 0) {
             alert("Nothing to move.");
             e.preventDefault();
@@ -64,7 +90,7 @@ function init($scope, $http, DataServiceApi) {
         $(selectedOpts).remove();
         e.preventDefault();
     };
-   
+
 }
 
 
